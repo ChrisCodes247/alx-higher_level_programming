273402392c6059be9e9a6fb78f057bf4773ae4bc
christopher <officialchris003@gmail.com>
2024-01-20 04:57:07 -0800

    fixed comments

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-#2-print_alphabet.py
-#chris
-
-''' Write a program that prints the ASCII alphabet, in lowercase, not followed by a new line.'''
-for i in range(97,123):
-    print("{}".format(chr(i), end = ""))
+# 2-print_alphabet.py
+# chris
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
