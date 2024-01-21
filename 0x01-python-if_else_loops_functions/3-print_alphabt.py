137e1f7f002ad9a0e5d9e1a853cd8e72127c1545
christopher <officialchris003@gmail.com>
2024-01-21 00:54:45 -0800

    modified the code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join(chr(c) for c in range(97, 123) if chr(c) not in "qe"),end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
