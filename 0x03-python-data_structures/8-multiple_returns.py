f194c1fecebb43d89e12093da8334435093fcad9
christopher <officialchris003@gmail.com>
2024-01-27 18:28:00 -0800

    corrected task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,6 @@
 def mutliple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
-    if sentence[0] != 0:
-        return length, first
+    if sentence == "":
+        return (0, None)
+    return length, first
