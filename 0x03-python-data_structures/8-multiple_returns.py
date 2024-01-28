5af3dc6ab9cb24fcabe59ede2f7337bb2c93212f
christopher <officialchris003@gmail.com>
2024-01-27 18:38:19 -0800

    edited task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def mutliple_returns(sentence):
-    if sentence[0] != None:
-        return(len(sentence), sentence[0])
-    else:
-        return(0, None)
+    if sentence == "":
+        return (0, None)
+    return(len(sentence), sentence[0])
