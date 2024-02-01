ad50f005b67733742531942fa26b1a448b6b9540
christopher <officialchris003@gmail.com>
2024-02-01 04:55:26 -0800

    removed and added task 1 again

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def search_replace(my_list, search, replace):
-    '''replace all occurences of an element in a list'''
 
+def search_replace(my_list, search, replace):
+    """Replace all occurrences of an element by another in a new list."""
     new_list = my_list[:]
-    for i in range(len(new_List)):
+    for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace
     return (new_list)
