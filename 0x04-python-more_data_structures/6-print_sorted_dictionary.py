45e214fcfafe7b0e7f5877284e033009690bc99d
christopher <officialchris003@gmail.com>
2024-02-01 17:49:41 -0800

    completed task 7-8

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+
+    '''Prints a dictionary by ordered keys.'''
+
+    for i, j in sorted(a_dictionary.items()):
+        print("{}{} {}".format(i, ":", j))
