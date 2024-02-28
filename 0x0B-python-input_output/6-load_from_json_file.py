a9d3e0eadbdb0ffb4b6f89ffcf28325b285b9b88
christopher <officialchris003@gmail.com>
2024-02-28 00:00:24 -0800

    Done task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,5 @@ import json
 def load_from_json_file(filename):
     """Write an object to a text file using JSON representation."""
     with open(filename, "w") as f:
-        json.load(f)
+        data = f.read()
+        return json.loads(data)
