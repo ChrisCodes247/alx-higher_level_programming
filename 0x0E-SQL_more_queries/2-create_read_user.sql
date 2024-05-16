65799cfc31d7b5d42469e466094bcd84f751297d
Christopher <officialchris003@gmail.com>
2024-05-16 11:53:04 -0700

    Done task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,9 +2,9 @@
 -- create the MySQL server user 'user_0d_2'
 -- grant only SELECT privilege
 
-CREATE DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER
+CREATE USER IF NOT EXISTS
 'user_0d_2'@'localhost'
 IDENTIFIED BY
 'user_0d_2_pwd';
