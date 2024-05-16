21a574d7e38abf5883ecf977801a68430c641dae
Christopher <officialchris003@gmail.com>
2024-05-16 12:26:48 -0700

    Done task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create database and table
 
 CREATE DATABASE IF NOT EXISTS  hbtn_0d_usa;
-
+USE hbtn_0d_usa;
 CREATE TABLE
 IF NOT EXISTS states(
 	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
