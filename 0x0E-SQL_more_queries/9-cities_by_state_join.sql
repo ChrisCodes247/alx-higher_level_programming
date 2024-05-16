af49a455b2377696b7d7c98b6956d9ab9c6e1418
Christopher <officialchris003@gmail.com>
2024-05-16 13:26:06 -0700

    Done task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,8 @@
 -- lists all cities contained in the database
 
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
+select
+cities.id, cities.name, states.name
+from cities, states
+where cities.state_id = states.id
+order by cities.id asc;
 
