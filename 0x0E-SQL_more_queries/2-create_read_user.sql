6c4749a6e5248e201a7eb5ed854cf7d2a1eb3160
Christopher <officialchris003@gmail.com>
2024-05-16 11:44:51 -0700

    Done task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- create database hbtn_0d_2
 -- create the MySQL server user 'user_0d_2'
 -- grant only SELECT privilege
+
 CREATE USER
 'user_0d_2'@'localhost'
 IDENTIFIED BY
@@ -8,5 +9,5 @@ IDENTIFIED BY
 
 GRANT SELECT
 ON hbtn_0d_2
-TO 'user_0d_1'@'localhost';
+TO 'user_0d_2'@'localhost';
 
