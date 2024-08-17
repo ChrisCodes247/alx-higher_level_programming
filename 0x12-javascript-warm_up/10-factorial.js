aa15a9fc8b92ebbd44772228f9d0d9f02e329df2
chrischege <chegechris959@gmail.com>
2024-08-17 17:50:10 +0300

    Done task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const number = parseInt(process.argv[2]);
+
+function factorial(n) {
+	if (n < 0) {
+		return 1;
+	}
+
+	if (n === 0 || n === 1) {
+		return 1;
+	}else {
+	return n * factorial(n - 1);
+	}
+}
+
+if (isNaN(number) || number < 0) {
+	console.log(1);
+} else {
+	console.log(factorial(number));
+}
