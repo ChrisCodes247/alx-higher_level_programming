6600f1fd9e753249d53e539271e21c47411a2831
chrischege <chegechris959@gmail.com>
2024-08-28 00:17:36 +0300

    Modified task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,6 @@
 import sys
 import MySQLdb
 
-print(sys.path)
 
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host='localhost', port=3306)
