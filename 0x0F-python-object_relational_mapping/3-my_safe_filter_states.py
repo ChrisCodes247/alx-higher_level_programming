b89c5b725c600cbd5bb443883fe4c24f994d7ad0
chrischege <chegechris959@gmail.com>
2024-08-28 01:25:22 +0300

    Done task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+    A script that lists all states that starts with a name
+    The name is inputted as an argument
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host='localhost', port=3306)
+
+    cursor = db.cursor()
+
+    search_name = sys.argv[4]
+
+    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
+    
+    cursor.execute(quwry, (search_name,))
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
